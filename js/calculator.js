429794564125d09e43123fca62b04a874b5e8014
petern1953 <petern1953@gmail.com>
2020-11-26 17:54:09 +0100

    html-váz kész

---
+++ js/calculator.js
@@ -0,0 +1,14 @@
+// Követelmények:
+// Az eval metódus használata SZIGORÚAN TILOS! Most és mindörökké!
+// Egyelőre nem kell foglalkozni azzal az esettel, hogy mi történik,
+// ha két műveleti jel van egymás után. Ilyen esetekben dobhattok hibát.
+// A felső input mezőben jelenjen meg az ERROR szöveg.
+// Egymás után több művelet is végrehajtható, pl.: 10 - 20 + 3 * 2.
+// Ilyen esetben a precedencia szabályokra még nem kell odafigyeled,
+// csak balról jobbra, sorban értékelődjenek ki a műveletek!
+// A számok és műveleti jelek a felső input mezőben jelenjenek meg.
+// Az egyenlőségjelre kattintva az inputban megjelenik a művelet(ek) eredménye
+// A C gomb törli az input mező tartamát
+
+'use strict'
+
